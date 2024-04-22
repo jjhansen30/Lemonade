d06c7430ba5c03648b92ad3fe4ebf6125cd1a3cc
Joshua Hansen <jjhansen30@gmail.com>
2024-04-22 10:44:48 -0600

    encompass column in to scafold composable

--- app/src/main/java/com/tutorial/mylemonade/ui/screens/LemonadeApp.kt
+++ app/src/main/java/com/tutorial/mylemonade/ui/screens/LemonadeApp.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,24 +17,35 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.tutorial.mylemonade.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LemonadeApp(viewModel: LemonadeAppViewModel) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = stringResource(R.string.lemonade)) })
+        },
+        modifier = Modifier.padding(16.dp)
     ) {
-        Image(
-            modifier = Modifier.clickable { viewModel.onImageClick() },
-            painter = painterResource(id = viewModel.image.intValue),
-            contentDescription = stringResource(id = viewModel.contentDescription.intValue)
-        )
-        Text(
-            modifier = Modifier.padding(16.dp),
-            text = stringResource(id = viewModel.userInstructions.intValue)
-        )
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(it),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.clickable { viewModel.onImageClick() },
+                painter = painterResource(id = viewModel.image.intValue),
+                contentDescription = stringResource(id = viewModel.contentDescription.intValue)
+            )
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = stringResource(id = viewModel.userInstructions.intValue)
+            )
+        }
     }
+
 }
 
 @Preview(showSystemUi = true)
